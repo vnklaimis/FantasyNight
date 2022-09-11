a9565dcc5b2cdbdef25f3d25921c3e59ed2deb6a
vnklaimis <61155333+vnklaimis@users.noreply.github.com>
2022-09-11 11:54:58 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -39,7 +39,7 @@ extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ['.templates']
 
 source_parsers = {
     '.md': CommonMarkParser,
